46f2228561d488c56f7e047b64b9b1e41583de9c
Janith Chamodya Warapitiya <warapitiyalakshan@gmail.com>
2026-07-17 20:34:39 +0530

    updated Home.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import Home from './pages/Home'
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
 
   return (
     <>
-      <h1>Hello world!</h1>
+     <Home />
     </>
   )
 }
